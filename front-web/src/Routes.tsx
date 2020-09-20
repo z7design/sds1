1830cb68ef6c13c58086637e16101e0bca1083b2
Jose Mario Ramos <z7design@z7design.com.br>
2020-09-19 22:40:34 -0300

    Adcionando a pagina de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/home';
 import Records from './pages/Records';
+import Charts from './pages/Charts';
 
 const Routes = () => (
 
@@ -16,6 +17,9 @@ const Routes = () => (
             </Route>
             <Route path="/records">
                <Records />
+            </Route>
+            <Route path="/charts">
+               <Charts />
             </Route>
         </Switch>
     </BrowserRouter>
---
+++ front-web/src/pages/Charts/types.ts
@@ -0,0 +1,10 @@
+import { Platform } from "../Records/types";
+
+export type Game = {
+
+    id: number,
+    title: string;
+    platform: Platform;
+
+}
+
